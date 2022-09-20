9baa3ef63f824f32ef38193ec0567468e55b11e8
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-09-20 18:49:37 -0400

    added the rest of the models

---
+++ migrations/20220920222604-create-stage.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('stages', {
+      stage_id: {
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      stage_name: {
+        allowNull: false
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('stages')
+  }
+};
